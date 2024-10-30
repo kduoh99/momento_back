84df23f081fac03a2bbd0a0fee37092f8ab49f26
Kang Duoh <kduoh99@gmail.com>
2024-10-30 22:30:30 +0900

    Deploy: 배포 스크립트 작성 (#9)

---
+++ src/main/java/com/hackathon/momento/global/ProfileController.java
@@ -0,0 +1,31 @@
+package com.hackathon.momento.global;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Hidden
+@RestController
+public class ProfileController {
+
+    private final Environment env;
+
+    public ProfileController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
